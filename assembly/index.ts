926bf8efb0ae23d498ff3cd287418238bec6511c
ahakanzn <37963260+ahakanzn@users.noreply.github.com>
2022-04-18 13:31:13 +0300

    bug and error fixes

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,103 +1,26 @@
-import { logging, context, ContractPromiseBatch, PersistentVector, u128, storage, Storage, MapEntry } from "near-sdk-as";
+import { Context } from "near-sdk-as";
 import { GuessGame, GameState, games } from "./model";
 
-export function createGame(): u32 {
-  const game = new GuessGame();
-  game.creator = context.sender;
-  game.gameState = GameState.Created;
-
-  games.set(game.gameId, game);
-  logging.log("yaratılan: " + games.get(game.gameId)!.toString());
-  return game.gameId;
+export function createGame(): GuessGame {
+  return GuessGame.createGame(Context.sender);
 }
 
-export function startGame(gameId: u32): string {
-  assert(games.contains(gameId), "There is no such a game, please create one first!");
-  const game = games.get(gameId)!;
-  assert(game.creator == context.sender, "You have not created this game thus you can't start it!");
-  //assert(game.gameState != GameState.Created, "Game is already started!");
-  game.gameState = GameState.InProgress;
-  games.set(game.gameId, game);
-  return `Game has started: ${gameId}`;
+export function startGame(gameId: u32): GuessGame {
+  return GuessGame.startGame(gameId);
 }
 
-export function makeGuess(gameId: u32): string {
-  assert(games.contains(gameId), "Game does not exists!");
-  logging.log("game id: " + gameId.toString());
-
-  const game = games.get(gameId)!;
-  //assert(game.creator != context.sender, "You can not join your own game!");
-  //assert(game.gameState != GameState.InProgress, "Game is not started yet or already finished!");
-  assert(!context.attachedDeposit.isZero || !isNull(context.attachedDeposit), "You have to add some deposit to play the game!");
-
-  game.guesses.push(context.attachedDeposit);
-  let attachedDeposit = context.attachedDeposit;
-
-  let mapVal = game.guessMap.get(attachedDeposit);
-  if (mapVal) {
-    mapVal.push(context.sender);
-    game.guessMap.set(attachedDeposit, mapVal);
-  } else {
-    mapVal = new PersistentVector<string>("s");
-    mapVal.push(context.sender);
-    game.guessMap.set(attachedDeposit, mapVal);
-  }
-  game.awardAmount = u128.add(game.awardAmount, context.attachedDeposit);
-  game.gameState = GameState.InProgress;
-
-  games.set(gameId, game);
-  return "You have made your guess!";
+export function makeGuess(gameId: u32): GuessGame {
+  return GuessGame.makeGuess(gameId);
 }
 
 export function finishGame(gameId: u32): string {
-  assert(games.contains(gameId), "There is no such a game, please create one first!");
-  const game = games.get(gameId)!;
-  assert(game.creator == context.sender, "You have not created this game thus you can't finish it!");
-  game.gameState = GameState.Completed;
-  let winnerId = context.sender;
-  let arr = bubbleSort(game.guesses, game.guesses.length);
-  //for (let i = 0; i < arr.length; i++) {
-  logging.log("arr: " + arr[0].toString());
-  //logging.log("arr: " + arr[1].toString());
-  /*let g = game.guessMap.get(u128.from(i));
-    logging.log("guess map: " + g!.first.toString());
-    if (g != null && g.length == 1) winnerId = g.first;*/
-  //}
-  /*
-  assert(!isNull(winnerId), "No one won the game!");
-  const to_winner = ContractPromiseBatch.create(winnerId);
-  const amount_to_receive = game.awardAmount;
-  to_winner.transfer(amount_to_receive);
-
-  games.set(game.gameId, game);*/
-  const amount_to_receive = 5;
-  return `Congratulations: ${winnerId} is the winner and received ${amount_to_receive}`;
+  return GuessGame.finishGame(gameId);
 }
 
-function swap(arr: PersistentVector<u128>, xp: i32, yp: i32): void {
-  var temp = arr[xp];
-  arr[xp] = arr[yp];
-  arr[yp] = temp;
+export function getGames(start: u32, limit: u32): GuessGame[] {
+  return GuessGame.findGames(start, limit);
 }
 
-// An optimized version of Bubble Sort
-function bubbleSort(arr: PersistentVector<u128>, n: i32): PersistentVector<u128> {
-  var i: i32, j: i32;
-  for (let i = 0; i < n - 1; i++) {
-    for (j = 0; j < n - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        swap(arr, j, j + 1);
-      }
-    }
-  }
-  return arr;
-}
-
-export function getGuess(gameId: u32): void {
-  let game = games.get(gameId)!;
-  let i: i32;
-  for (i = 0; i < game.guesses.length; i++) {
-    logging.log(game.guesses.pop().toString());
-  }
-  logging.log("lenght: " + game.guesses.length.toString());
+export function getGame(gameId: u32): GuessGame {
+  return GuessGame.findGameById(gameId);
 }
--- assembly/model.ts
+++ assembly/model.ts
@@ -1,4 +1,4 @@
-import { RNG, context, PersistentMap, u128, PersistentVector, storage } from "near-sdk-as";
+import { RNG, Context, u128, PersistentVector, PersistentUnorderedMap, ContractPromiseBatch } from "near-sdk-as";
 
 export enum GameState {
   Created,
@@ -11,9 +11,8 @@ export class GuessGame {
   gameId: u32;
   gameState: GameState;
   awardAmount: u128;
-  creator: string;
-  guessMap: PersistentMap<u128, PersistentVector<string>>;
-  guesses: PersistentVector<u128>;
+  creator: string = Context.sender;
+  guessMap: PersistentUnorderedMap<u128, PersistentVector<string>> = new PersistentUnorderedMap<u128, PersistentVector<string>>("map");
 
   constructor() {
     let rng = new RNG<u32>(1, u32.MAX_VALUE);
@@ -22,27 +21,82 @@ export class GuessGame {
 
     this.gameState = GameState.Created;
     this.awardAmount = u128.Zero;
-    this.creator = context.sender;
-    this.guessMap = new PersistentMap<u128, PersistentVector<string>>("gMap");
-    this.guesses = new PersistentVector<u128>("guesses");
+    this.creator = Context.sender;
   }
 
-  toString(): string {
-    return (
-      "GuessGame{" +
-      "gameId=" +
-      this.gameId.toString() +
-      ", gameState='" +
-      this.gameState.toString() +
-      "'" +
-      ", awardAmount=" +
-      this.awardAmount.toString() +
-      ", creator='" +
-      this.creator +
-      "'" +
-      "}"
-    );
+  static createGame(creator: string): GuessGame {
+    const game = new GuessGame();
+    game.gameState = GameState.Created;
+    games.set(game.gameId, game);
+    return game;
+  }
+
+  static startGame(gameId: u32): GuessGame {
+    assert(games.contains(gameId), "There is no such a game, please create one first!");
+    const game = games.get(gameId)!;
+
+    assert(game.creator == Context.sender, "You have not created this game thus you can't start it!");
+    assert(game.gameState != GameState.InProgress, "Game is already started!");
+
+    game.gameState = GameState.InProgress;
+    games.set(game.gameId, game);
+    return game;
+  }
+
+  static makeGuess(gameId: u32): GuessGame {
+    assert(games.contains(gameId), "Game does not exists!");
+    const game = games.get(gameId)!;
+
+    assert(game.creator != Context.sender, "You can not join your own game!");
+    assert(game.gameState != GameState.InProgress, "Game is not started yet or already finished!");
+    assert(!Context.attachedDeposit.isZero || !isNull(Context.attachedDeposit), "You have to add some deposit to play the game!");
+
+    let attachedDeposit = Context.attachedDeposit;
+    let mapVal = game.guessMap.get(attachedDeposit, new PersistentVector<string>("new"));
+    if (!isNull(mapVal) || mapVal!.isEmpty) {
+      mapVal!.push(Context.sender);
+      game.guessMap.set(attachedDeposit, mapVal!);
+    } else {
+      mapVal = new PersistentVector<string>("s");
+      mapVal.push(Context.sender);
+      game.guessMap.set(attachedDeposit, mapVal);
+    }
+    game.awardAmount = u128.add(game.awardAmount, Context.attachedDeposit);
+    games.set(gameId, game);
+    return game;
+  }
+
+  static finishGame(gameId: u32): string {
+    assert(games.contains(gameId), "There is no such a game, please create one first!");
+
+    const game = games.get(gameId)!;
+    assert(game.creator == Context.sender, "You have not created this game thus you can't finish it!");
+
+    game.guessMap.keys(0, game.guessMap.length).sort();
+    let winner = game.guessMap
+      .values(0, game.guessMap.length)
+      .reduce((a: PersistentVector<string>, b: PersistentVector<string>) => (a.length <= b.length ? a : b), game.guessMap.values(0, 1).at(0));
+    let winnerId = winner.first.toString();
+    assert(!isNull(winnerId), "No one won the game!");
+
+    const to_winner = ContractPromiseBatch.create(winnerId);
+    const amount_to_receive = game.awardAmount;
+    to_winner.transfer(amount_to_receive);
+
+    game.gameState = GameState.Completed;
+    games.set(game.gameId, game);
+    return `Congratulations: ${winnerId} is the winner and received ${amount_to_receive}`;
+  }
+
+  static findGames(offset: u32, limit: u32 = 10): GuessGame[] {
+    return games.values(offset, limit + offset);
+  }
+
+  static findGameById(id: u32): GuessGame {
+    return games.getSome(id);
   }
 }
 
-export const games = new PersistentMap<u32, GuessGame>("games");
+export const games = new PersistentUnorderedMap<u32, GuessGame>("games");
+
+// An optimized version of Bubble Sort
